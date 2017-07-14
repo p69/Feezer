361d3a4fbf937d20c4382b72019a0ff5f5578849
pavel-shilyagov <pshilyagov@viber.com>
2017-07-14 19:25:31 +0300

    Added namespace for server.

--- src/Server/Program.fs
+++ src/Server/Program.fs
@@ -1,36 +1,39 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿namespace Feezer.Server
 
-open System
-open System.Threading
-open Feezer.Domain.User
-open Suave
-open Suave.Filters
-open Suave.Operators
-open Suave.Successful
+[<AutoOpen>]
+module Server =
 
-[<EntryPoint>]
-let main argv =
-    let permissions = Authorization.Email <||> Authorization.Basic <||> Authorization.DeleteLibrary <||> Authorization.ManageLibrary <||> Authorization.OfflineAccess
-    let uri = Authorization.buildLoginUri "app_id" "http://localhost:8080/auth" permissions
+  open System
+  open System.Threading
+  open Feezer.Domain.User
+  open Suave
+  open Suave.Filters
+  open Suave.Operators
+  open Suave.Successful
 
-    let app =
-      choose
-        [
-          GET >=> choose
-            [ path "/hello" >=> OK "hi"
-              path "/bye" >=> OK "good bye"]
-          POST >=> choose
-            [ path "/post" >=> OK "dasda"]
-        ]
+  [<EntryPoint>]
+  let main argv =
+      let permissions = Authorization.Email <||> Authorization.Basic <||> Authorization.DeleteLibrary <||> Authorization.ManageLibrary <||> Authorization.OfflineAccess
+      let uri = Authorization.buildLoginUri "app_id" "http://localhost:8080/auth" permissions
 
-    let cts = new CancellationTokenSource()
-    let conf = { defaultConfig with cancellationToken = cts.Token }
-    let listening, server = startWebServerAsync conf app
+      let app =
+        choose
+          [
+            GET >=> choose
+              [ path "/hello" >=> OK "hi"
+                path "/bye" >=> OK "good bye"]
+            POST >=> choose
+              [ path "/post" >=> OK "dasda"]
+          ]
 
-    Async.Start(server, cts.Token)
-    printfn "Feezer server is started..."
-    Console.ReadKey true |> ignore
-    printfn "Stop feezer server"
-    cts.Cancel()
+      let cts = new CancellationTokenSource()
+      let conf = { defaultConfig with cancellationToken = cts.Token }
+      let listening, server = startWebServerAsync conf app
 
-    0 // return an integer exit code
+      Async.Start(server, cts.Token)
+      printfn "Feezer server is started..."
+      Console.ReadKey true |> ignore
+      printfn "Feezer server is stopped"
+      cts.Cancel()
+
+      0 // return an integer exit code
