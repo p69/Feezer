f5bec4ac759723e3dd0e28fcc836bdcc9848a54c
pavel-shilyagov <shilyagov.p@gmail.com>
2017-07-17 02:00:40 +0300

    Try to use Fable.Websockets
    https://github.com/ncthbrt/Fable.Websockets

--- src/Client/src/Renderer/Renderer.fs
+++ src/Client/src/Renderer/Renderer.fs
@@ -1,6 +1,7 @@
 module Feezer.Client.Renderer
 
 open Fable.Import
+open Fable.Websockets.Client
 
 let body = Browser.document.getElementById("app")
 body.textContent <- "Hello Feezer!"
--- src/Server/Program.fs
+++ src/Server/Program.fs
@@ -10,15 +10,43 @@ module Server =
   open Suave.Filters
   open Suave.Operators
   open Suave.Successful
+  open Suave.WebSocket
+  open Suave.Sockets
+  open Suave.Sockets.Control
 
   [<EntryPoint>]
   let main argv =
       let permissions = Authorization.Email <||> Authorization.Basic <||> Authorization.DeleteLibrary <||> Authorization.ManageLibrary <||> Authorization.OfflineAccess
       let uri = Authorization.buildLoginUri "app_id" "http://localhost:8080/auth" permissions
 
+      let ws (webSocket:WebSocket) (context:HttpContext) =
+        socket {
+          let mutable loop = true
+
+          while loop do
+            let! msg = webSocket.read()
+
+            match msg with
+            | (Text, data, true) ->
+                let str = UTF8.toString data
+                let response = sprintf "response to %s" str
+                let byteResponse =
+                    response
+                    |> System.Text.Encoding.ASCII.GetBytes
+                    |> ByteSegment
+                do! webSocket.send Text byteResponse true
+
+            | (Close, _, _) ->
+                let emptyResponse = [||] |> ByteSegment
+                do! webSocket.send Close emptyResponse true
+                loop <- false
+
+            | _ -> ()
+        }
+
       let app =
-        choose
-          [
+        choose [
+            path "/websocket" >=> handShake ws
             GET >=> choose
               [ path "/hello" >=> OK "hi"
                 path "/bye" >=> OK "good bye"]
